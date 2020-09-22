35c88ecc71b5880a59cb93cc98f9a873ecc2d72b
Aditya Verma <aaadarths2357@gmail.com>
2020-09-22 16:58:52 +0530

    Use Case 5 : Calculating Wages for a Month

--- EmpWage.java
+++ EmpWage.java
@@ -3,24 +3,28 @@ public class EmpWage{
 	public static final int isFullTime=1;
 	public static final int isPartTime=2;
 	public static final int wagePerHour=20;
+	public static final int numberOfWorkingDays=20;
 
 	public static void main(String[] args){
 
 		int empHours=0;
 		int empWage=0;
-
-		int empCheck=(int)Math.floor(Math.random()*10)%3;
-		switch(empCheck){
-			case isFullTime:
-				empHours=8;
-				break;
-			case isPartTime:
-				empHours=4;
-				break;
-			default:
-			empHours=0;
+		int monthlyEmpWage=0;
+		for(int day =0; day<numberOfWorkingDays; day++){
+			int empCheck=(int)Math.floor(Math.random()*10)%3;
+			switch(empCheck){
+				case isFullTime:
+					empHours=8;
+					break;
+				case isPartTime:
+					empHours=4;
+					break;
+				default:
+				empHours=0;
+			}
+			empWage=empHours*wagePerHour;
+			monthlyEmpWage+=empWage;
 		}
-		empWage=empHours*wagePerHour;
-		System.out.println("Employee Wage : "+empWage);
+		System.out.println("Monthly Employee Wage : "+monthlyEmpWage);
 	}
 }

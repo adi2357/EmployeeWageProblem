9e7a42c39918e347161fec0578f306b4c4aeec39
Aditya Verma <aaadarths2357@gmail.com>
2020-09-22 16:46:25 +0530

    Use Case 4 : Solving Using Switch Case Statement

--- EmpWage.java
+++ EmpWage.java
@@ -8,14 +8,18 @@ public class EmpWage{
 
 		int empHours=0;
 		int empWage=0;
-		
+
 		int empCheck=(int)Math.floor(Math.random()*10)%3;
-		if (empCheck==isFullTime)
-			empHours=8;
-		else if(empCheck==isPartTime)
-			empHours=4;
-		else
+		switch(empCheck){
+			case isFullTime:
+				empHours=8;
+				break;
+			case isPartTime:
+				empHours=4;
+				break;
+			default:
 			empHours=0;
+		}
 		empWage=empHours*wagePerHour;
 		System.out.println("Employee Wage : "+empWage);
 	}

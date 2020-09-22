c96a09a3da9e42c20d280979cc42f03fdd521c0d
Aditya Verma <aaadarths2357@gmail.com>
2020-09-22 16:13:21 +0530

    Use Case 1 : Check Employee Presence

--- EmpWage.java
+++ EmpWage.java
@@ -2,6 +2,11 @@ public class EmpWage{
 
 	public static void main(String[] args){
 
-		System.out.println("Welcome to Employee Wage Computation Program");
+		int isFullTime=1;
+		int empCheck=(int)Math.floor(Math.random()*10)%2;
+		if (empCheck==isFullTime)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
